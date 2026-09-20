94e70bd65c15d4282633252083013992cd95fee7
Mohit <227001593+mks19992019-sudo@users.noreply.github.com>
2026-09-20 10:15:53 +0530

    Time: 222 ms (66.93%) | Memory: 31.4 MB (9.38%) - LeetSync

--- leet-code/200-number-of-islands/number-of-islands.py
+++ leet-code/200-number-of-islands/number-of-islands.py
@@ -5,39 +5,38 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+
         m = len(grid)
         n = len(grid[0])
-        visited = set()
-        count = 0
-
+        Visited = set()
+        No_Islands =0
         directions = [
             (-1,0),#up
             (0,-1),#left
-            (0,1),#right
-            (1,0)#down
+            (1,0),#down
+            (0,1)#right
         ]
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and (i,j) not in visited:
-                    count +=1
+                if grid[i][j] == '1' and (i,j) not in Visited:
+                    No_Islands +=1
                     q = deque()
                     q.append((i,j))
-
-                    visited.add((i,j))
+                    Visited.add((i,j))
 
                     while q:
-                        raw_ , col_ = q.popleft()
+                        raw, col = q.popleft()
 
+                        for raw_ , col_ in directions:
 
-                        for dr ,  dc in directions:
-                            new_raw = raw_ + dr
-                            new_col = col_ + dc
+                            new_raw = raw + raw_ 
+                            new_col = col + col_
 
-                            if (0<= new_raw < m) and (0<=new_col<n)and grid[new_raw][new_col]=='1'and (new_raw,new_col) not in visited :
-                                visited.add((new_raw, new_col))
-                                q.append((new_raw, new_col))
-        return count
+                            if (0<= new_raw < m) and (0<=new_col<n) and grid[new_raw][new_col] =="1" and (new_raw,new_col) not in Visited:
+                                q.append((new_raw,new_col))
+                                Visited.add((new_raw,new_col))
+        return No_Islands
 
 
 
