5f6e2a24cdb79ad7a0cc9a52d14203743a3f2db5
Mohit <227001593+mks19992019-sudo@users.noreply.github.com>
2026-09-23 19:32:06 +0530

    Time: 211 ms (32.34%) | Memory: 15.9 MB (51.73%) - LeetSync

--- leet-code/542-01-matrix/01-matrix.py
+++ leet-code/542-01-matrix/01-matrix.py
@@ -5,38 +5,42 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: List[List[int]]
         """
-        grid = mat
+        m = len(mat)
+        n = len(mat[0])
 
-        directions = [
-            (-1,0),#up
-            (1,0),#down
-            (0,-1),#left
-            (0,1)#right 
-        ]
-
-        visited = set()
         q = deque()
-
-        m = len(grid)
-        n = len(grid[0])
+        visited = set()
+        
+        direction = [
+            (-1,0),#up
+            (0,-1),#left
+            (0,1),#right
+            (1,0)#down
+        ]
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==0:
+                if mat[i][j] == 0:
                     q.append((i,j))
                     visited.add((i,j))
         
         while q:
             raw , col = q.popleft()
-            for raw_ , col_ in directions:
-                nr = raw+raw_
-                nc = col + col_
+            for raw_ , col_ in direction:
+                new_raw = raw + raw_
+                new_col = col + col_
 
-                if 0<=nr<m and 0<=nc<n and (nr,nc) not in visited:
-                    grid[nr][nc] = grid[raw][col] + 1
-                    q.append((nr,nc))
-                    visited.add((nr,nc))
-        return grid
+                if (0<= new_raw<m) and (0<=new_col<n) and (new_raw ,new_col) not in visited:
+                    mat[new_raw][new_col] = mat[raw][col] + 1
+                    q.append((new_raw,new_col))
+                    visited.add((new_raw,new_col))
+        return mat
+
+
+        
+
+
+       
 
         
 
