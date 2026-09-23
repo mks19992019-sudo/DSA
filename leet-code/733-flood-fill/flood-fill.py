d5ef03d176c18365bc4b24711b2142139378efab
Mohit <227001593+mks19992019-sudo@users.noreply.github.com>
2026-09-23 22:13:44 +0530

    Time: 3 ms (28.89%) | Memory: 12.7 MB (38.91%) - LeetSync

---
+++ leet-code/733-flood-fill/flood-fill.py
@@ -0,0 +1,50 @@
+from collections import deque
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """ 
+        m = len(image)
+        n = len(image[0])
+        direction =[
+            (-1,0),
+            (0,-1),
+            (0,1),
+            (1,0)
+        ]
+
+        current_color = image[sr][sc]
+
+        q = deque()
+        visited = set()
+
+        q.append((sr,sc))
+        visited.add((sr,sc))
+        image[sr][sc] = color
+
+        while q:
+            raw , col = q.popleft()
+
+            for r_ , c_ in direction:
+                nr = raw+r_
+                nc = col+c_
+
+                if (0<=nr<m) and (0<=nc<n) and image[nr][nc]==current_color and (nr,nc) not in visited:
+                    image[nr][nc] = color
+                    q.append((nr,nc))
+                    visited.add((nr,nc))
+        return image
+
+
+
+
+
+
+
+
+
+        
